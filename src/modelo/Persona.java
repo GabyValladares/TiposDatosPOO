93d5275d5369fa7c88437a186ccd9332075ee8d5
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-05-10 14:58:44 -0500

    Clases, atributos y objetos, métodos

---
+++ src/modelo/Persona.java
@@ -0,0 +1,44 @@
+package modelo;
+
+/**
+ *
+ * @author PEpito Pérez
+ */
+//1.-NOMBRE DE LA CLASE:
+public class Persona {
+//2.-ATRIBUTOS ->DECLARACIÓN GLOBAL DE VARIABLES
+    //public-private-protected
+    public int idPersona;
+    public String nombres;
+    public String apellidos;
+    public String etnia;
+    public int cedula;
+    public int numeroTelefono;
+ //3.-MÉTODOS/ACCIONES/COMPORTAMIENTOS
+     //public-private
+    //1)MÉTODO DE NO RETORNO
+    //SON SECCIONES DE CÓDIGO
+    //PUEDEN POSEER VARIABLES LOCALES
+    public void saludar(){
+        //MOSTRAR INFORMACIÓN RECURRIR A IMPRIMIR
+        System.out.println("HOLA:"+nombres);
+    }
+    //1.1)MÉTODO DE NO RETORNO UTILIZAR ARGUMENTOS DE ENTRADA
+    //UN ARGUMENTO ES UNA VARIABLE LOCAL
+    //QUE SE DECLARA DENTRO DE LOS ()
+    //SOLO SE RECONOCE EN ESE BLOQUE DE CÓDIGO
+    public void saludarEdad(int edad){
+        if(edad>18){
+            System.out.println("Hola Mayor de Edad");
+        }else{
+            System.out.println("Hola Menor de Edad");
+            }
+    }
+}//MÉTODO QUE NOS INDIQUE EL HORÓSCOPO 
+//public void saludarEdad(String edad){
+//        if(edad=="Aries"){
+//            System.out.println("Hola Mayor de Edad");
+//        }else{
+//            System.out.println("Hola Menor de Edad");
+//            }
+//    }
---
+++ src/vista/Main1.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package vista;
+
+import modelo.Persona;
+
+/**
+ *
+ * @author Usuario
+ */
+//1.-CONSUMIENTO OBJETOS
+public class Main1 {
+    //2.-LLAMAR A MÉTODO MAIN TRADUCTOR 
+    //O INTERPRETE DEL CÓDIGO 
+    //PROBAR EL CÓDIGO
+    
+    public static void main(String[] args) {
+        //1.-CREAR/INSTANCIAR/DECLARAR OBJETO
+        //NombreClase nombreObjeto=new NombreClase();
+        Persona gaby=new Persona();
+        //2.-DARLE/INCIALIZARLE DATOS
+        //nombreObjeto.atributo/variable="dato";
+       //ACCEDIENDO A LOS ATRIBUTOS
+        gaby.idPersona=001;
+        gaby.nombres="Gabriela";
+        gaby.apellidos="Valladares";
+        gaby.etnia="mestiza";
+        gaby.cedula=1003297792;
+        gaby.numeroTelefono=992937736;
+        //ACCEDIENDO A LOS MÉTODOS
+        gaby.saludar();
+        //CONSUMIENTO MÉTODO (EL DATO DENTRO DE LOS PARÉNTESIS SE LLAMA PARÁMETROS ) 
+        gaby.saludarEdad(1);
+    }
+}
