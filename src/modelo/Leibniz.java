a9f7146331f17d2754bb4c6f680c140f75704db9
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-05-13 15:04:20 -0500

    Resultado Double e Impresion serie

--- src/modelo/Leibniz.java
+++ src/modelo/Leibniz.java
@@ -14,32 +14,36 @@ public class Leibniz {
         //acumulador:almacenar información incremental
         double res=0;
         //auxiliar de iteración
-        int denom=1;
+        double denom=1;
         //constante
         final int NUM=4;
         //bandera:signo 1->sumar y 2->restar
         int signo=1;
+        //concatenar serie
+        String serie="";
         //iteración
         for (int i = 0; i < num; i++) {
             if(signo==1){
             //res+=(NUM/denom)+res;
-            res=(NUM/denom)+res;
+            res+=(NUM/denom);
+            serie+="(+"+NUM+""+"/"+denom+")";
                 signo=2;
                 denom+=2;
             }else{
-                res=res-(NUM/denom);
+                res-=(NUM/denom);
+                serie+="(-"+NUM+""+"/"+denom+")";
                 signo=1;
                 denom+=2;
             }
         }
         
-        System.out.println("La aproximación es:"+res);
+        System.out.println(serie+"\n"+"La aproximación es:"+res);
     
     }
     
     public static void main(String[] args) {
         Leibniz prueba1=new Leibniz();
-        prueba1.calcularLeibniz(7);
+        prueba1.calcularLeibniz(3);
     }
     
 }
