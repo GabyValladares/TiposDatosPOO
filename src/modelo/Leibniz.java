9f99c2fe51984d344e921097cd3cb8a4ce75fe4b
OrtegaMoncayo <richard.ortega778@ist17dejulio.edu.ec>
2024-05-13 15:09:01 -0500

    intento

--- src/modelo/Leibniz.java
+++ src/modelo/Leibniz.java
@@ -9,37 +9,43 @@ package modelo;
  * @author Usuario
  */
 public class Leibniz {
+
     //GENERAR UN MÉTODO DE NO RETORNO CON ARGUMENTO QUE PERMITA CALCULAR LEIBNIZ
-    public void calcularLeibniz(int num){
+    public void calcularLeibniz(int num) {
         //acumulador:almacenar información incremental
-        double res=0;
+        double res = 0;
         //auxiliar de iteración
-        int denom=1;
+        int denom = 1;
         //constante
-        final int NUM=4;
+        final int NUM = 4;
         //bandera:signo 1->sumar y 2->restar
-        int signo=1;
+        int signo = 1;
         //iteración
         for (int i = 0; i < num; i++) {
-            if(signo==1){
-            //res+=(NUM/denom)+res;
-            res=(NUM/denom)+res;
-                signo=2;
-                denom+=2;
-            }else{
-                res=res-(NUM/denom);
-                signo=1;
-                denom+=2;
+            System.out.println(""+ NUM + "/" + denom);
+            if (signo == 1) {
+                System.out.println("suma=" + NUM + "/" + denom);
+                //res+=(NUM/denom)+res;
+                res = (NUM / denom) + res;
+                signo = 2;
+                denom += 2;
+            } else {
+                System.out.println("resta=" + NUM + "/" + denom);
+                res = res - (NUM / denom);
+                signo = 1;
+                denom += 2;
             }
         }
         
-        System.out.println("La aproximación es:"+res);
-    
+        
+
+        System.out.println("La aproximación es:" + res);
+
     }
-    
+
     public static void main(String[] args) {
-        Leibniz prueba1=new Leibniz();
+        Leibniz prueba1 = new Leibniz();
         prueba1.calcularLeibniz(7);
     }
-    
+
 }
