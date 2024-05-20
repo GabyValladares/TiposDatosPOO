406c6367d1134467c700f6282d0484b05a4c5120
GabyValladares <126087266+GabyValladares@users.noreply.github.com>
2024-05-20 15:08:15 -0500

    clase planeta con encapsulamiento

--- src/modelo/Planeta.java
+++ src/modelo/Planeta.java
@@ -38,5 +38,67 @@ public class Planeta {
         this.observable=observable;
     }   
     //3.ENCAPSULAMIENTO
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String n){
+        this.nombre=n;
+    }
+    
+    public int getCantidadSatelites(){
+        return cantidadSatelites;
+    }
+    public void setCantidadSatelites(int n){
+        this.cantidadSatelites=n;
+    }
+      public int getDiametro(){
+        return diametro;
+    }
+    public void setDiametro(int n){
+        this.diametro=n;
+    }
+      public double getMasa(){
+        return masa;
+    }
+    public void setMasa(int n){
+        this.masa=n;
+    }
+    public double getVolumen(){
+        return volumen;
+    }
+    public void setVolumen(int n){
+        this.volumen=n;
+    }
+      public int getDistanciaSol(){
+        return distanciaSol;
+    }
+    public void setDistanciaSol(int n){
+        this.distanciaSol=n;
+    }
+    
+    public String getTipoPlaneta(){
+        return tipoPlaneta;
+    }
+    public void setTipoPlaneta(String n){
+        this.tipoPlaneta=n;
+    }
+    public boolean getObservable(){
+        return observable;
+    }
+    public void setObservable(boolean n){
+        this.observable=n;
+    }
+    
+    public void imprimir(){
+        System.out.println("Datos Planeta\n"+
+                "Nombre:"+getNombre()+"\n"+
+                "Cantidad Satélitas"+getCantidadSatelites()+"\n"+
+                "Masa:"+getMasa()+"\n"+
+                "Volumen:"+getVolumen()+"\n"+
+                "Diámetro:"+getDiametro()+"\n"+
+                "Distancia al Sol:"+getDistanciaSol()+"\n"+
+                "Tipo de Planeta:"+getTipoPlaneta()+"\n"+
+                "¿Es Observable?:"+getObservable());
+    }
     
 }
